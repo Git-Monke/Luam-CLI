55f89840d4bde5c9e275b193c03d6107a10e294b
Git-Monke <javelasq2006@gmail.com>
2024-01-15 13:47:53 -0800

    Removed deletion step from actions

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 touch release/temp.lua
 cd ./src
 FILES=$(find . -name '*.lua' | grep -v 'luam.lua' | cut -c3- | sed "s/\.lua$//" | sed "s/\//\./g")
-luacc -o ../release/temp.lua luam $FILES 
+luacc -o ../release/verbose.lua luam $FILES 
 cd ..
 
-luamin -f release/temp.lua > release/luam.lua
+luamin -f release/verbose.lua > release/luam.lua
 luamin -f installation_script.lua > release/install.lua
 
 rm release/temp.lua
