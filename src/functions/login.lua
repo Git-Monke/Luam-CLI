8db70619a7d0d2a61475fe339da85995b6be12ee
Git-Monke <javelasq2006@gmail.com>
2024-01-15 21:53:53 -0800

    Minor change to luam login command

--- src/functions/login.lua
+++ src/functions/login.lua
@@ -1,9 +1,6 @@
 local function login(args)
-    print("API Token: ")
-    local token = io.read()
-
     local key_writer = fs.open("luam.key", "w")
-    key_writer.write(token)
+    key_writer.write(args[2])
     key_writer.close()
 
     return "API token now in use"
